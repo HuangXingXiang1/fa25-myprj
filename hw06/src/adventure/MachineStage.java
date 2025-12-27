ba0270b1a55f0b22d54d322c28db8aa0aa4c76e3
HuangXingXiang1 <3517561952@qq.com>
2025-12-28 00:36:01 +0800

    Hw06-Commit

--- hw06/src/adventure/MachineStage.java
+++ hw06/src/adventure/MachineStage.java
@@ -112,28 +112,28 @@ public class MachineStage implements AdventureStage {
      * and b = {0, 20, 5}, this function will return {1, 20, 5}.
      */
     public static int[] arrayMax(int[] a, int[] b) {
-        if (a.length != b.length) {
-            System.out.println("ERROR! Array lengths don't match");
-            return null;
-        }
-        int[] returnArray = new int[a.length];
-        for (int i = 0; i < a.length; i += 1) {
-            int biggerValue = mysteryMax(a[i], b[i]);
-            returnArray[i] = biggerValue;
-        }
-
-        return returnArray;
+       if (a.length != b.length){
+           System.out.println("Error! Array length don't match");
+           return null;
+       }
+       int[] temp = new int[a.length];
+       for (int i = 0, length = a.length; i < length; i++){
+            if (a[i] > b[i]) {
+                temp[i] = a[i];
+            }else {
+                temp[i] = b[i];
+            }
+       }
+       return temp;
     }
 
     /**
      * Returns the sum of all elements in x.
      */
     public static int arraySum(int[] x) {
-        int i = 0;
         int sum = 0;
-        while (i < x.length) {
-            sum = sum + mysteryAdd(sum, x[i]);
-            i = i + 1;
+        for (int i = 0, length = x.length; i < length; i++) {
+            sum += x[i];
         }
         return sum;
     }
