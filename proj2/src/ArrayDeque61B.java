3431b92163e6bf22cbabb606972d9efa4ad130b8
HuangXingXiang1 <3517561952@qq.com>
2026-01-03 15:32:30 +0800

    proj2-commit

---
+++ proj2/src/ArrayDeque61B.java
@@ -0,0 +1,275 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ArrayDeque61B<T> implements Deque61B<T>{
+
+    private T[] content;
+    private int nextFirst;
+    private int nextLast;
+    private int size;
+    public int length;
+
+    public ArrayDeque61B() {
+        content = (T[]) new Object[8];
+        nextFirst = 7;
+        nextLast = 0;
+        size = 0;
+    }
+
+    private class WrapIterator implements Iterator<T>{
+        int current = 0;
+        /**
+         * Returns {@code true} if the iteration has more elements.
+         * (In other words, returns {@code true} if {@link #next} would
+         * return an element rather than throwing an exception.)
+         *
+         * @return {@code true} if the iteration has more elements
+         */
+        @Override
+        public boolean hasNext() {
+            return current < size;
+        }
+
+        /**
+         * Returns the next element in the iteration.
+         *
+         * @return the next element in the iteration
+         * throws NoSuchElementException if the iteration has no more elements
+         */
+        @Override
+        public T next() {
+            int index = nexthelper((nextFirst+1+current) % content.length);
+            current++;
+            return content[index];
+        }
+    }
+    /**
+     * Add {@code x} to the front of the deque. Assumes {@code x} is never null.
+     *
+     * @param x item to add
+     */
+    @Override
+    public void addFirst(T x) {
+        if (size == content.length){
+            expansion();
+        }
+        content[nextFirst] = x;
+        nextFirst = nexthelper(nextFirst -1);
+        size++;
+    }
+
+    /**
+     * Add {@code x} to the back of the deque. Assumes {@code x} is never null.
+     *
+     * @param x item to add
+     */
+    @Override
+    public void addLast(T x) {
+        if (size == content.length){
+            expansion();
+        }
+        content[nextLast] = x;
+        nextLast = nexthelper(nextLast+1);
+        size++;
+    }
+
+    //扩容
+    private void expansion() {
+        T[] expansion = (T[]) new Object[size * 2];
+        length = expansion.length;
+        for (int i = 0; i < size; i++) {
+            // 尾部开始添加原数组的首项直到填充完毕
+            // 保证在逻辑上是循环数组， 并且以顺时针为开头访问
+            expansion[(expansion.length-1+i) % expansion.length] = content[(nextFirst +1+i) % content.length];
+        }
+        // 新的首位指针会位于第一位之前
+        nextFirst = expansion.length-2;
+        // 新的末尾指针会位于最后一位之后
+        nextLast = size - 1;
+        content = expansion;
+    }
+
+    /**
+     * Returns a List copy of the deque. Does not alter the deque.
+     *
+     * @return a new list copy of the deque.
+     */
+    @Override
+    public List<T> toList() {
+        List<T> returnlist = new ArrayList<>();
+        if (size == 0){
+            return returnlist;
+        }
+        for(int i = 0; i < size; i++){
+            T element = content[(nextFirst +1+i) % content.length];
+            returnlist.addLast(element);
+        }
+        return returnlist;
+    }
+
+    /**
+     * Returns if the deque is empty. Does not alter the deque.
+     *
+     * @return {@code true} if the deque has no elements, {@code false} otherwise.
+     */
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * Returns the size of the deque. Does not alter the deque.
+     *
+     * @return the number of items in the deque.
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Remove and return the element at the front of the deque, if it exists.
+     *
+     * @return removed element, otherwise {@code null}.
+     */
+    @Override
+    public T removeFirst() {
+        if (size == 0) {
+        return null;
+        }
+        int tempnext = nexthelper(nextFirst +1);
+        return removehelper(tempnext, 1);
+    }
+
+    /**
+     * Remove and return the element at the back of the deque, if it exists.
+     *
+     * @return removed element, otherwise {@code null}.
+     */
+    @Override
+    public T removeLast() {
+        if (size == 0) {
+        return null;
+        }
+        int templast = nexthelper(nextLast - 1);
+        return removehelper(templast, 0);
+    }
+
+    private T removehelper(int templast, int stip) {
+        T deletvalue = content[templast];
+        content[templast] = null;
+        if (stip == 1) {
+            nextFirst = templast;
+        }else {
+            nextLast = templast;
+        }
+        size--;
+        float ratio = ( (float) size / content.length);
+        if (ratio <= 0.25 && content.length >= 16){
+            shrinkage();
+        }
+        return deletvalue;
+    }
+
+    private void shrinkage() {
+        int shrinkage = (int) ((size / 0.30));
+        T [] container;
+        container = (T[]) new Object[shrinkage];
+        for (int i = 0; i < size; i++) {
+            container[(container.length-1+i) % container.length] = content[(nextFirst +1+i) % content.length];
+        }
+        // 首部指针永远位于首值之前
+        nextFirst = container.length-2;
+        // 尾部指针永远位于尾部之后
+        nextLast = size - 1;
+        content = container;
+        length = content.length;
+    }
+
+    // 循环辅助函数：处理索引越界
+    private int nexthelper(int index) {
+        if (index == content.length) {
+            return 0;
+        }
+        if (index < 0) {
+            return content.length - 1;
+        }
+        return index;
+    }
+
+    /**
+     * The Deque61B abstract data type does not typically have a get method,
+     * but we've included this extra operation to provide you with some
+     * extra programming practice. Gets the element, iteratively. Returns
+     * null if index is out of bounds. Does not alter the deque.
+     *
+     * @param index index to get
+     * @return element at {@code index} in the deque
+     */
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size){
+            return null;
+        }
+        return content[(index+ nextFirst +1) % content.length];
+    }
+
+    /**
+     * This method technically shouldn't be in the interface, but it's here
+     * to make testing nice. Gets an element, recursively. Returns null if
+     * index is out of bounds. Does not alter the deque.
+     *
+     * @param index index to get
+     * @return element at {@code index} in the deque
+     */
+    @Override
+    public T getRecursive(int index) {
+        throw new UnsupportedOperationException("No need to implement getRecursive for ArrayDeque61B.");
+    }
+
+    /**
+     * Returns an iterator over elements of type {@code T}.
+     *
+     * @return an Iterator.
+     */
+    @Override
+    public Iterator<T> iterator() {
+        return new WrapIterator();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o instanceof ArrayDeque61B updateo) {
+            if (size != updateo.size) {
+                return false;
+            }
+            for (int i = 0; i < size; i++) {
+                if (get(i) != updateo.get(i)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        int count = 0;
+        StringBuilder display = new StringBuilder();
+        display.append("[");
+        for (T x : this) {
+            count++;
+            display.append(x);
+            if (count != size) {
+                display.append(", ");
+            }
+        }
+        display.append("]");
+        return display.toString();
+    }
+}
--- proj2/tests/ArrayDeque61BTest.java
+++ proj2/tests/ArrayDeque61BTest.java
@@ -1,6 +1,263 @@
-import org.junit.jupiter.api.DisplayName;
+import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.google.common.truth.Truth.assertThat;
 
 public class ArrayDeque61BTest {
+    @Test
+    public void addFirstTebxBasic() {
+        ArrayDeque61B<Integer> lld1 = new ArrayDeque61B<>();
+        lld1.addFirst(12);
+        assertThat(lld1.toList()).containsExactly(12).inOrder();
+        lld1.addFirst(13);
+        assertThat(lld1.toList()).containsExactly(13, 12).inOrder();
+        lld1.addFirst(14);
+        assertThat(lld1.toList()).containsExactly(14, 13, 12).inOrder();
+        lld1.addFirst(15);
+        assertThat(lld1.toList()).containsExactly(15 ,14, 13, 12).inOrder();
+        assertThat(lld1.size()).isEqualTo(4);
+    }
+    @Test
+    public void addLastTebxBasic() {
+        ArrayDeque61B<Integer> lld1 = new ArrayDeque61B<>();
+        lld1.addLast(12);
+        assertThat(lld1.toList()).containsExactly(12).inOrder();
+        lld1.addLast(13);
+        assertThat(lld1.toList()).containsExactly(12, 13).inOrder();
+        lld1.addLast(14);
+        assertThat(lld1.toList()).containsExactly(12, 13, 14).inOrder();
+        lld1.addLast(15);
+        assertThat(lld1.toList()).containsExactly(12, 13, 14, 15).inOrder();
+        assertThat(lld1.size()).isEqualTo(4);
+    }
+
+    @Test
+    public void addLastANDaddFristTebxBasic() {
+        ArrayDeque61B<Integer> lld1 = new ArrayDeque61B<>();
+        lld1.addLast(12);
+        assertThat(lld1.toList()).containsExactly(12).inOrder();
+        lld1.addFirst(19);
+        assertThat(lld1.toList()).containsExactly(19, 12).inOrder();
+        lld1.addLast(13);
+        assertThat(lld1.toList()).containsExactly( 19, 12, 13).inOrder();
+
+    }
+
+
+    @Test
+    public void getTextBasic() {
+        ArrayDeque61B<String> lld1 = new ArrayDeque61B<>();
+        assertThat(lld1.get(0)).isEqualTo(null);
+        assertThat(lld1.get(-1)).isEqualTo(null);
+        lld1.addLast("身体健康");
+        assertThat(lld1.get(0)).isEqualTo("身体健康");
+        lld1.addFirst("新年快乐");
+        assertThat(lld1.get(0)).isEqualTo("新年快乐");
+        assertThat(lld1.get(1)).isEqualTo("身体健康");
+        lld1.addLast("万事如意");
+        assertThat(lld1.get(2)).isEqualTo("万事如意");
+        lld1.addFirst("天天开心");
+        lld1.addFirst("平安喜乐");
+        assertThat(lld1.get(0)).isEqualTo("平安喜乐");
+        assertThat(lld1.get(1)).isEqualTo("天天开心");
+        lld1.addLast("福如东海");
+        assertThat(lld1.get(5)).isEqualTo("福如东海");
+        assertThat(lld1.get(-1)).isEqualTo(null);
+        assertThat(lld1.get(6)).isEqualTo(null);
+        lld1.addLast(null);
+        assertThat(lld1.get(6)).isEqualTo(null);
+    }
+
+    @Test
+    public void isEmptyTextBasic() {
+        ArrayDeque61B<Integer> lld1 = new ArrayDeque61B<>();
+        assertThat(lld1.isEmpty()).isTrue();
+        lld1.addLast(10);
+        assertThat(lld1.isEmpty()).isFalse();
+        lld1.removeFirst();
+        assertThat(lld1.isEmpty()).isTrue();
+    }
+
+    @Test
+    public void getSizeTextBasic() {
+        ArrayDeque61B<String > lld1 = new ArrayDeque61B<>();
+        assertThat(lld1.size()).isEqualTo(0);
+        lld1.addFirst("平安喜乐");
+        lld1.addFirst("平安喜乐");
+        lld1.addFirst("平安喜乐");
+        lld1.addFirst("平安喜乐");
+        lld1.addFirst("平安喜乐");
+        assertThat(lld1.size()).isEqualTo(5);
+        lld1.addFirst("身体健康");
+        assertThat(lld1.size()).isEqualTo(6);
+        lld1.removeFirst();
+        lld1.removeFirst();
+        lld1.removeFirst();
+        lld1.removeFirst();
+        lld1.removeFirst();
+        lld1.removeFirst();
+        assertThat(lld1.size()).isEqualTo(0);
+    }
+
+    @Test
+    public void toListTextBasic() {
+        ArrayDeque61B<Integer> lld1 = new ArrayDeque61B<>();
+        List<Integer> lld0 = new ArrayList<>();
+        assertThat(lld1.toList()).isEqualTo(lld0);
+        lld1.addFirst(10);
+        lld1.addLast(11);
+        lld1.addLast(12);
+        lld1.addFirst(15);
+        assertThat(lld1.toList()).containsExactly(15, 10, 11, 12).inOrder();
+        lld1.addFirst(11);
+        lld1.addFirst(10);
+        lld1.addLast(10);
+        assertThat(lld1.toList()).containsExactly(10, 11, 15, 10 ,11, 12, 10).inOrder();
+    }
+
+    @Test
+    public void removeFirstTextBasic() {
+        ArrayDeque61B<Integer> lld1 = new ArrayDeque61B<>();
+        assertThat(lld1.removeFirst()).isEqualTo(null);
+        lld1.addLast(10); // [10]
+        assertThat(lld1.removeFirst()).isEqualTo(10);
+        List<Integer> lld0 = new ArrayList<>();
+        assertThat(lld1.toList()).isEqualTo(lld0);
+        lld1.addFirst(11);
+        lld1.addLast(123);
+        lld1.addFirst(19);
+        lld1.addFirst(20); // [20, 19, 11, 123]
+        assertThat(lld1.removeFirst()).isEqualTo(20);
+        assertThat(lld1.toList()).containsExactly(19, 11, 123).inOrder();
+        assertThat(lld1.size()).isEqualTo(3);
+    }
+
+    @Test
+    public void removeLastTextBasic() {
+        ArrayDeque61B<Integer> lld1 = new ArrayDeque61B<>();
+        assertThat(lld1.removeLast()).isEqualTo(null);
+        lld1.addFirst(10); // [10]
+        assertThat(lld1.removeLast()).isEqualTo(10);
+        List<Integer> lld0 = new ArrayList<>();
+        assertThat(lld1.toList()).isEqualTo(lld0);
+        lld1.addFirst(11);
+        lld1.addLast(123);
+        lld1.addFirst(19);
+        lld1.addFirst(20);
+        assertThat(lld1.removeLast()).isEqualTo(123);
+        assertThat(lld1.toList()).containsExactly(20, 19, 11).inOrder();
+        assertThat(lld1.size()).isEqualTo(3);
+    }
+
+    @Test
+    public void expansionTextBasic() {
+        ArrayDeque61B<Integer> lld1 = new ArrayDeque61B<>();
+        List<Integer> text = new ArrayList<>();
+        for (int i = 0; i < 101; i++) {
+            lld1.addFirst(i);
+            text.addFirst(i);
+        }
+        assertThat(lld1.toList()).isEqualTo(text);
+        for (int i = 200; i < 301; i++) {
+            lld1.addLast(i);
+            text.addLast(i);
+        }
+        assertThat(lld1.toList()).isEqualTo(text);
+        assertThat(lld1.get(0)).isEqualTo(text.get(0));
+        assertThat(lld1.get(200)).isEqualTo(text.get(200));
+        assertThat(lld1.get(5)).isEqualTo(text.get(5));
+        assertThat(lld1.get(39)).isEqualTo(text.get(39));
+        for (int i = 0; i < 100; i++) {
+            lld1.removeFirst();
+            text.removeFirst();
+        }
+        assertThat(lld1.toList()).isEqualTo(text);
+        for (int i = 0; i < 10000000; i++) {
+            lld1.addFirst(i);
+        }
+        lld1.get(999999);
+    }
+
+    @Test
+    public void shrinkageTextBasic() {
+        ArrayDeque61B<Integer> lld1 = new ArrayDeque61B<>();
+        for (int i = 0; i < 16; i++) {
+            lld1.addFirst(i);
+        }
+        for (int i = 0; i < 15; i++) {
+            lld1.removeFirst();
+        }
+        for (int i = 0; i < 100000; i++) {
+            lld1.addFirst(i);
+        }
+        for (int i = 0; i < 99999; i++) {
+            lld1.removeLast();
+        }
+        assertThat(lld1.length).isEqualTo(13);
+    }
+
+    @Test
+    public void iterationTextBasic() {
+        ArrayDeque61B<String> lld1 = new ArrayDeque61B<>();
+        lld1.addFirst("深远");
+        lld1.addFirst("平静");
+        lld1.addFirst("智慧");
+        assertThat(lld1).containsExactly("智慧", "平静", "深远");
+        ArrayDeque61B<Integer> lld2 = new ArrayDeque61B<>();
+        lld2.addLast(10);
+        lld2.addLast(11);
+        lld2.addLast(12);
+        lld2.addLast(13);
+        lld2.addLast(14);
+        lld2.addLast(15);
+        lld2.addLast(16);
+        lld2.addLast(17);
+        lld2.addLast(18);
+        lld2.addLast(19);
+        assertThat(lld2).containsExactly(10, 11, 12, 13, 14, 15, 16, 17, 18, 19);
+    }
+
+    @Test
+    public void testEqualDeques61B() {
+        Deque61B<String> ad = new ArrayDeque61B<>();
+        Deque61B<String> ad2 = new ArrayDeque61B<>();
+
+        ad.addLast("front");
+        ad.addLast("middle");
+        ad.addLast("back");
+
+        ad2.addLast("front");
+        ad2.addLast("middle");
+        ad2.addLast("back");
+
+        assertThat(ad).isEqualTo(ad2);
+    }
+
+    @Test
+    public void toStringTextBasic() {
+        ArrayDeque61B<String> lld1 = new ArrayDeque61B<>();
+        assertThat(lld1.toString()).isEqualTo("[]");
+        lld1.addFirst("深远");
+        lld1.addFirst("平静");
+        lld1.addFirst("智慧");
+        assertThat(lld1.toString()).isEqualTo("[智慧, 平静, 深远]");
+        ArrayDeque61B<Integer> lld2 = new ArrayDeque61B<>();
+        lld2.addLast(10);
+        lld2.addLast(11);
+        lld2.addLast(12);
+        lld2.addLast(13);
+        lld2.addLast(14);
+        lld2.addLast(15);
+        lld2.addLast(16);
+        lld2.addLast(17);
+        lld2.addLast(18);
+        lld2.addLast(19);
+        assertThat(lld2.toString()).isEqualTo("[10, 11, 12, 13, 14, 15, 16, 17, 18, 19]");
+        String lld3 = "2e";
+
+    }
 }
+
